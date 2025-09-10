ef3c76ef7eecb4036da3a2cf2627137507b2e9b9
Ananda S RPi <4833102+asarangaram@users.noreply.github.com>
2025-09-10 21:48:20 +0530

    remove unwanted prints and loggers

--- app/src/ai_session/events.py
+++ app/src/ai_session/events.py
@@ -11,14 +11,12 @@ logging.basicConfig(level=logging.INFO)
 def register_ai_session_events(*, socket: SocketIO, model: AISessionManager):
     @socket.on("connect")
     def handle_connect():
-        print(session)
         sid = request.sid
         model.create_session(sid)
         logging.info(f"Client connected: {sid}")
 
     @socket.on("recognize")
     def handle_message(msg):
-        print(session)
         sid = request.sid
         model.update_activity(sid)
         logging.info(f"Recognize {msg}: received")
@@ -26,12 +24,9 @@ def register_ai_session_events(*, socket: SocketIO, model: AISessionManager):
             logging.info(f"Recognize {msg}: succeeded")
         else:
             logging.info(f"Recognize {msg}: failed")
-            
 
     @socket.on("disconnect")
     def handle_disconnect():
-        print(session)
         sid = request.sid
         logging.info(f"Client disconnected: {sid}")
         model.remove_client(sid)
-
--- app/src/common/config.py
+++ app/src/common/config.py
@@ -5,17 +5,14 @@ import logging
 
 from .get_unique_id import get_unique_device_id
 
-logger = logging.getLogger(__name__)
+
 
 env_file = "../env/.env"
+
 # Inside docker,  if relative path doesn't work
 # This won't work on regular run
 if not os.path.exists(env_file):
     env_file = "/env/.env"
-
-""" with open(env_file, 'r') as file:
-        for line in file:
-            print(line, end='') """
 
 if not load_dotenv(env_file):
     raise Exception("Failed to load environment ")
@@ -23,7 +20,6 @@ if not load_dotenv(env_file):
 
 def get_required_env_variable(var_name):
     value = os.environ.get(var_name)
-
     if value is None:
         raise ValueError(f"Environment variable {var_name} is not set.")
     return value
@@ -33,7 +29,4 @@ class ConfigClass(object):
     UPLOAD_STORAGE_LOCATION = get_required_env_variable("UPLOAD_STORAGE_LOCATION")
     APP_SECRET = get_required_env_variable("APP_SECRET")
     HOST_NAME = get_required_env_variable("HOST_NAME")
-    logging.warning(f"HOSTNAME:{HOST_NAME}")
     APP_NAME = "ai." + get_unique_device_id(HOST_NAME)
-    logging.warning(f"APP_NAME {APP_NAME}")
-
