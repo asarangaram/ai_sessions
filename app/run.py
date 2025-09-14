e3a3dc8e04dfadad2a08b551a8a8cf5bea6752f3
Ananda S RPi <4833102+asarangaram@users.noreply.github.com>
2025-09-14 08:12:16 +0530

    server - not available

--- app/run.py
+++ app/run.py
@@ -5,4 +5,4 @@ import logging
 logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
 if __name__ == "__main__":
-    socketio.run(application, host="0.0.0.0", port=5002, server="eventlet")
+    socketio.run(application, host="0.0.0.0", port=5002)
